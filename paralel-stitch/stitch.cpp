b97a1afb1f2e5ac526f5b5bd5511d8394a417907
rohmanwidiyanto <widiyanto.rohman@gmail.com>
2017-05-19 06:42:03 +0700

    finised yaaayyy

--- paralel-stitch/stitch.cpp
+++ paralel-stitch/stitch.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <iostream>
 #include <fstream>
+#include <mpi.h>
 #include "opencv2/gpu/gpu.hpp"
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/stitching/stitcher.hpp"
@@ -9,6 +10,24 @@ using namespace std;
 using namespace cv;
 
 int main(int argc, char* argv[]) {
+	MPI_Init(NULL, NULL);
+
+  // Get the number of processes
+  int world_size;
+  MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+  // Get the rank of the process
+  int world_rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+	// Get the name of the processor
+  char processor_name[MPI_MAX_PROCESSOR_NAME];
+  int name_len;
+  MPI_Get_processor_name(processor_name, &name_len);
+
+	// Print off a hello world message
+  printf("Processing from processor %s, rank %d out of %d processors\n", processor_name, world_rank, world_size);
+
 	vector< Mat > vImg;
 
 	vector< vector< Rect > > vvRect;
@@ -42,4 +61,6 @@ int main(int argc, char* argv[]) {
 	waitKey(0);
 
 	imwrite("output.jpg", rImg);
+
+	MPI_Finalize();
 }
